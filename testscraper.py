b1050016ab6522c19d9f581f5e8eabda88c8abba
Crexodon <github@crexy.moe>
2025-10-05 00:19:52 +0200

    Added function to transform line_id formats, changed database connection handling, renamed bus scraping to vehicle scraping, added while true loop to scrape data automatically

--- testscraper.py
+++ testscraper.py
@@ -1,17 +1,16 @@
-import sqlite3, requests, logging, re, urllib.parse
+import sqlite3, requests, logging, re, urllib.parse, time, sys
 
-bus_lines = ["j25-hnv-30001-R-E", "j25-hnv-30001-H-E"]
-s4_lines = ["j25-hnv-100S4-R-"]
-bus_url = "https://www3.vvs.de/mnghnv/VELOC?"
-bus_params = dict(
+vehicle_url = "https://www3.vvs.de/mnghnv/VELOC?"
+vehicle_params = dict(
     CoordSystem = 'WGS84',
     LineID = '',
-    #coordOutputFormat = 'EPSG:4326',
+    coordOutputFormat = 'EPSG:4326',
     outputFormat = 'rapidJSON',
     serverInfo = '1'
 )
 
-station_lines = ["hnv:30001:E:R:j25:6", "hnv:30001:E:H:j25:6"]
+station_version = '6'
+station_lines = ["hnv:30001:E:R:j25", "hnv:30001:E:H:j25", "hnv:100S4: :H:j25", "hnv:100S4: :R:j25"]
 station_url = "https://www3.vvs.de/mnghnv/XML_GEOOBJECT_REQUEST?"
 station_params = dict(
     SpEncId = '0',
@@ -42,6 +41,8 @@ timetable_params = dict(
 )
 
 stop_reg = re.compile(r'^.*?(?=#)')
+
+conn = sqlite3.connect('testdb.db')
 
 def create_database():
     conn = sqlite3.connect('testdb.db')
@@ -94,15 +95,13 @@ def create_database():
     conn.close()
 
 # Line ID has a :6 at the end!
-def get_stations(bus_line):
-    station_params['line'] = bus_line
+def get_stations(station_line):
+    station_params['line'] = station_line + ':' + station_version
     station_params_str = urllib.parse.urlencode(station_params, safe=':')
     resp = requests.get(url=station_url, params=station_params_str)
     data: dict = resp.json()
 
     sequences = data['transportations'][0]['locationSequence']
-
-    conn = sqlite3.connect('testdb.db')
 
     for station in sequences:
 
@@ -126,7 +125,6 @@ def get_stations(bus_line):
         c.execute(sql, tuple(station_parse.values()))
 
         conn.commit()
-    conn.close()
 
 # TODO Needs further adjustment, Weekday, Saturday, Sun and Holiday timetables            
 def get_timetables(station_id):
@@ -153,71 +151,109 @@ def get_timetables(station_id):
 
         print(dict(time_parse))
 
-def scrape(bus_line):
-    bus_params['LineID'] = bus_line
-    resp = requests.get(url=bus_url, params=bus_params)
+# Transforms Line ID from Scrape to Station format and also the other way
+def transform_lineid(line_str):
+
+    if ':' in line_str:
+        match = re.match(r'^([^:]+):([^:]+):([^:]*):([^:]+):([^:]+)$', line_str)
+
+        if match:
+            line1 = match.group(1)    # hnv
+            line2 = match.group(2)    # 30001
+            dir2 = match.group(3)     # E or space
+            dir1 = match.group(4)     # R or H
+            prefix = match.group(5)   # j25
+            
+            # If pos2 is space, replace with empty string
+            if dir2 == ' ':
+                dir2 = ''
+            
+            # Reconstruct: prefix-part1-part2-pos1-pos2
+            return f"{prefix}-{line1}-{line2}-{dir1}-{dir2}"
+    
+    elif '-' in line_str:
+        match = re.match(r'^([^-]+)-(.+)-([^-]+)-([^-]*)$', line_str)
+        
+        if match:
+            prefix = match.group(1)  # j25
+            line = match.group(2)    # hnv-30001
+            dir1 = match.group(3)    # R or H
+            dir2 = match.group(4)    # E or empty string
+            
+            # If pos2 is empty, replace with space
+            if not dir2:
+                dir2 = ' '
+            
+            # Reconstruct: middle:number:pos2:pos1:prefix
+            # First replace the dash in middle with colon
+            line = line.replace('-', ':')
+            
+            # Build final string with swapped positions
+            return f"{line}:{dir2}:{dir1}:{prefix}"
+    
+    return None
+
+def scrape(vehicle_line):
+    vehicle_params['LineID'] = transform_lineid(vehicle_line)
+    vehicle_params_str = urllib.parse.urlencode(vehicle_params, safe=':')
+    resp = requests.get(url=vehicle_url, params=vehicle_params_str)
     data = resp.json()
 
-    conn = sqlite3.connect('testdb.db')
+    print('Current Vehicles on: ' + vehicle_line + ' = ' + str(len(data)))
 
-    # Convert line_id format from j25-hnv-30001-R-E to hnv:30001:R:E:j25
-    line_reg = re.sub(r'^(\w+)-(.*)', r'\2-\1', bus_line) # AI generated regex
-    line_reg = line_reg.replace('--', '-')
-    bus_line = line_reg.replace('-', ':')
-
-    print('Current Busses on: ' + bus_line + ' = ' + str(len(data)))
-
-    for bus in data:
-        print(bus['DirectionText'] + ", ID: " + bus['ID'])
+    for vec in data:
+        print(vec['DirectionText'] + ", ID: " + vec['ID'])
         
 
-        bus_parse = dict(
-            line_id = bus_line,
-            bus_id = bus['ID'],
-            bus_timestamp = bus['Timestamp'],
-            latitude = bus['Latitude'],
-            longitude = bus['Longitude'],
-            current_stop = stop_reg.match(bus['CurrentStop']).group(),
-            next_stop = stop_reg.match(bus['NextStop']).group(),
-            realtime_available = bus['RealtimeAvailable'],
-            current_delay = bus['Delay'],
-            bus_name = bus['LineText'],
-            bus_journey = bus['DirectionText']
+        vec_parse = dict(
+            line_id = vehicle_line,
+            bus_id = vec['ID'],
+            bus_timestamp = vec['Timestamp'],
+            latitude = vec['Latitude'],
+            longitude = vec['Longitude'],
+            current_stop = stop_reg.match(vec['CurrentStop']).group(),
+            next_stop = stop_reg.match(vec['NextStop']).group(),
+            realtime_available = vec['RealtimeAvailable'],
+            current_delay = vec['Delay'],
+            bus_name = vec['LineText'],
+            bus_journey = vec['DirectionText']
         )
 
         c = conn.cursor()
 
-        columns = ', '.join(bus_parse.keys())
-        placeholders = ', '.join('?' * len(bus_parse))
+        columns = ', '.join(vec_parse.keys())
+        placeholders = ', '.join('?' * len(vec_parse))
         sql = 'INSERT INTO busses ({}) VALUES ({})'.format(columns, placeholders)
-        c.execute(sql, tuple(bus_parse.values()))
+        c.execute(sql, tuple(vec_parse.values()))
 
         conn.commit()
 
-    conn.close()
-
-
-
-
-        
-
-
-
 if __name__ == '__main__':
-    import http.client as http_client
-    http_client.HTTPConnection.debuglevel = 1
+    # import http.client as http_client
+    # http_client.HTTPConnection.debuglevel = 1
 
-    # You must initialize logging, otherwise you'll not see debug output.
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
+    # #You must initialize logging, otherwise you'll not see debug output.
+    # logging.basicConfig()
+    # logging.getLogger().setLevel(logging.DEBUG)
+    # requests_log = logging.getLogger("requests.packages.urllib3")
+    # requests_log.setLevel(logging.DEBUG)
+    # requests_log.propagate = True
     
     create_database()
-    get_stations(station_lines[0])
+    for station_line in station_lines:
+        print('Getting Station Info for: ' + station_line)
+        get_stations(station_line)
+        time.sleep(5)
+    
     #get_timetables('de:08121:191:0:1')
-    scrape(bus_line=bus_lines[0])
 
-
-#FOREIGN KEY(trackartist) REFERENCES artist(artistid)
+    try:
+        while True:
+            for station_line in station_lines:
+                scrape(station_line)
+                time.sleep(2)
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt detected. Cleaning up and exiting...')
+        conn.close()
+        sys.exit()
+    
