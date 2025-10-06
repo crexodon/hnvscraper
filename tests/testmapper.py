24d94b855d76b79741af043b8a6ba4f482fd4781
Crexodon <github@crexy.moe>
2025-10-06 02:28:08 +0200

    tested fetch for realtime data access

--- tests/testmapper.py
+++ tests/testmapper.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 m = folium.Map(location=(49.126131, 9.270954), tiles="Cartodb Positron")
 
-conn = sqlite3.connect('testdb2.db')
+conn = sqlite3.connect('tests/testdb2.db')
 
 c = conn.cursor()
 
@@ -28,7 +28,42 @@ c.execute(select_query, ('8225#!ADD!#vdvserversws##!ADD!#DDIP-NVBW-VVS#', ))
 
 items = c.fetchall()
 
-c.close()
+# Testdata
+last_vecs = [
+    ("8223#!ADD!#vdvserversws##!ADD!#DDIP-NVBW-VVS#", "2025-10-05T16:57:23+02:00"),
+    ("85348-48899#!ADD!#vbk##!ADD!#DDIP-M_KVV##!ADD!#DDIP-NVBW-VVS#", "2025-10-05T16:57:23+02:00")
+]
+
+select_query = """SELECT latitude, longitude, bus_timestamp, current_stop, next_stop, realtime_available, current_delay, bus_name, bus_journey FROM vehicles WHERE trip_id = (?) AND bus_timestamp = (?);"""
+rets = []
+for last_vec in last_vecs:
+    c.execute(select_query, last_vec)
+    ret = c.fetchall()
+
+    c.execute("""SELECT name FROM stations WHERE stop_id = (?)""", (ret[0][3], ))
+    cur_stop = c.fetchall()
+
+    c.execute("""SELECT name FROM stations WHERE stop_id = (?)""", (ret[0][4], ))
+    nxt_stop = c.fetchall()
+    
+    ret_parse = dict(
+        geojson = dict(
+            type = "Point",
+            coordinates = [
+                ret[0][1],
+                ret[0][0]
+            ]
+        ),
+        timestamp = ret[0][2],
+        current_stop = cur_stop[0][0],
+        next_stop = nxt_stop[0][0],
+        realtime_available = str(ret[0][5]),
+        realtime_delay = str(ret[0][6]),
+        line_number = ret[0][7],
+        line_name = ret[0][8]
+    )
+
+    print(ret_parse)
 conn.close()
 
 
